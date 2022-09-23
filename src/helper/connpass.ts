ba2283d58ad9cfc5ef018c82e3558f2e3a0231be
2nofa11 <aboubthethirdkimi@icloud.com>
2022-09-24 00:32:19 +0900

    リファクタリング

--- src/helper/connpass.ts
+++ src/helper/connpass.ts
@@ -2,16 +2,20 @@ import axios from "axios";
 export type SearchParam = {
   keyword: string;
   count: number;
+  [key: string]: string | number;
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const eventListFromKeyword = async (searchParam: any) => {
+export const eventListFromKeyword = async (searchParam: SearchParam) => {
+  // パラメータをURLように整形
   const reqParamArr: string[] = [];
   for (const key in searchParam) {
     reqParamArr.push(`${key}=${searchParam[key]}`);
   }
   const reqParam = reqParamArr.join("&");
   console.log(reqParam);
+
+  // 検索実行
   const res = await axios.get(`https://connpass.com/api/v1/event/?${reqParam}`);
   return res.data;
 };
