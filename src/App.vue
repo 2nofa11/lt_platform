01a984473745bde062a340e6fe8e182bc80bff82
2nofa11 <aboubthethirdkimi@icloud.com>
2022-09-25 13:52:50 +0900

    側の設定

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
 <template>
   <v-app>
-    <v-main>
-      <router-view />
+    <v-app-bar app flat>
+      <v-container>
+        <v-btn> connpass </v-btn>
+      </v-container>
+    </v-app-bar>
+    <v-spacer></v-spacer>
+    <v-main color="">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list color="transparent">
+                <v-list-item v-for="n in 5" :key="n" link>
+                  <v-list-item-content>
+                    <v-list-item-title>List Item {{ n }}</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list></v-sheet
+            ></v-col
+          >
+          <v-col>
+            <v-sheet min-height="70vh" rounded="lg"> <router-view /></v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
