ab435621a98bf15e934ee16c092fc35e1234063c
2nofa11 <aboubthethirdkimi@icloud.com>
2022-09-27 20:51:37 +0900

    リファクタリング：サイドバーの外だし

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,10 @@
         <v-btn @click="backToHome"> HOME </v-btn>
       </v-container>
     </v-app-bar>
-    <v-main color="">
+    <v-main>
       <v-container>
         <v-row>
-          <v-col cols="2">
-            <v-sheet rounded="lg">
-              <v-list color="transparent">
-                <v-list-item v-for="n in 5" :key="n" link>
-                  <v-list-item-content @click="goToAbout('about', n)">
-                    <v-list-item-title>List Item {{ n }}</v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-list></v-sheet
-            ></v-col
-          >
+          <v-col cols="2"><SideBar /> </v-col>
           <v-col>
             <v-sheet min-height="70vh" rounded="lg"> <router-view /></v-sheet>
           </v-col>
@@ -30,11 +20,12 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import SideBar from "@/components/SideBar.vue";
 import router from "./router";
 
 export default defineComponent({
   name: "App",
-
+  components: { SideBar },
   data() {
     return {};
   },
---
+++ src/components/SideBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-sheet rounded="lg">
+    <v-list color="transparent">
+      <v-list-item v-for="n in 5" :key="n" link>
+        <v-list-item-content @click="changeRoute('about', n)">
+          <v-list-item-title>List Item {{ n }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-sheet>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import router from "@/router";
+
+export default defineComponent({
+  methods: {
+    changeRoute: function (name: string, keyword: string) {
+      router.push({
+        name: name,
+        params: { keyword: keyword },
+      });
+    },
+  },
+});
+</script>
