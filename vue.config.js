74d6b172c01415b93044adc257ac900bf5e87ea2
2nofa11 <aboubthethirdkimi@icloud.com>
2022-10-02 21:56:53 +0900

    GitHubPages

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  publicPath: "/lt_platform/",
+  outputDir: "docs",
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
